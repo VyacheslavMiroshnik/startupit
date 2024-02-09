54a9ff49665d352d1e2d9ead3f0fb428ad98aa84
Vyacheslav <mepa087@gmail.com>
2024-02-09 05:53:34 +0300

    add carts

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,26 +5,31 @@
 @endsection
 @section('content')
     <main>
+
+
         <div class="container">
             <h1 class="text-center mt-5">Корзина</h1>
             <div class="row mb-4">
+
+                @foreach($products as $product)
                 <div class="col-12 col-lg-8">
                     <article class="card mt-4 overflow-hidden">
                         <div class="row">
                             <div class="col-12 col-sm-4">
                                 <div class="img-wrap">
-                                    <img class="w-100" src="/images/pic-1.webp" alt="Изображение товара">
+                                    <img class="w-100" src="{{asset('storage/img')}}/{{$product->img}}" alt="Изображение товара">
                                 </div>
                             </div>
                             <div class="col-12 col-sm-8 d-flex align-items-center">
                                 <div class="p-3">
                                     <h3 class="fs-6 mb-2">
-                                        Шоссейный велосипед BMC Roadmachine 01 Five Ultegra Di2 (2023)
+                                       {{$product->description}}
                                     </h3>
-                                    <p>Кол-во - 3 шт.
+                                    <p>
+                                        {{$products->where('id',$product->id)->first()->pivot->count}}
                                     </p>
                                     <p class="fw-bold fs-6 m-0">
-                                        цена без скидки - 773 280 ₽ / шт.
+                                        цена без скидки - {{$product->price}} ₽ / шт.
                                     </p>
                                     <p class="fw-bold fs-6 m-0">
                                         с учётом скидки <span>5%</span> - 734 616 ₽ / шт.
@@ -34,6 +39,8 @@
                         </div>
                     </article>
                 </div>
+                @endforeach
+
                 <div class="col-12 col-lg-4">
                     <div class="card p-3 mt-4">
                         <p class="fs-4">Общая сумма заказа:</p>
